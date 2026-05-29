0e37355f8f5a5a0985c0be9874dcb09ed29672d1
seaavey <seaavey@users.noreply.github.com>
2026-05-29 18:38:55 +0700

    chore: remove unused test icon files

--- test_icon.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div class="p-10 flex gap-4">
-    <Icon name="hugeicons:star" class="text-yellow-500 fill-yellow-500" size="24" />
-    <Icon name="hugeicons:star" class="text-yellow-500 [&>path]:fill-current" size="24" />
-  </div>
-</template>
